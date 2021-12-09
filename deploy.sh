234cc732558de2b2a92b88dcbd3609a2eafc7e39
PX Slip (Will K.) <dev@pxslip.com>
2021-12-09 19:52:32 +0000

    Oops, committed the old token

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 helm upgrade --install \
---set botToken=NTUyNTM1NzA1MjMxMDMyMzQy.D2A8oQ.kLXLusnY0y_4m0S8o_vEOZwp48Y\
+--set botToken=$BOT_TOKEN\
 ,global.postgresql.postgresqlPassword=secret \
 --create-namespace \
 --namespace kickstarter-bot \
